38f430def68b565cfe4ceff78839fd9714d4b0e9
Ian Buckley <ianb@ionconcepts.com>
2025-04-14 11:37:13 -0700

    Carry Save Add WIP

---
+++ lib/dsp/carry_save_add.sv
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------------------
+// File:   carry_save_add.sv
+//
+// Author:  Ian Buckley, Ion Concepts LLC.
+//
+// Parameterizable:
+// * Width of datapath.
+//
+// Description:
+// Carry Save Adder (3->2 compressor).
+// 
+//
+// License: CERN-OHL-P (See LICENSE.md)
+//
+//-----------------------------------------------------------------------------
+`timescale 1ns/1ps
+
+module carry_save_add
+  #(
+    parameter WIDTH = 16  
+    )
+   (
+    input logic [WIDTH-1:0]  in0,
+    input logic [WIDTH-1:0]  in1,
+    input logic [WIDTH-1:0]  in2,    
+    //
+    output logic [WIDTH-1:0] sum_out, // bit weight [WIDTH-1:0]
+    output logic [WIDTH-1:0] carry_out // bit weight [WIDTH:1]
+    );
+
+   genvar		     i;
+   generate
+      for (i=0; i < WIDTH; i++) begin
+	 {carry_out[i],sum_out[1]} = in0[i] + in1[i] + in2[i];
+      end
+   endgenerate
+   
+   
+endmodule // carry_save_add
